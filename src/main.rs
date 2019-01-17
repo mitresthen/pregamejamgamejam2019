98458ab42153f73bdcbf776bc461f9243f603b37
Faxmachinen <mail@faxmachinen.com>
2019-01-17 20:09:05 +0100

    Added party.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,32 @@ use sdl2::pixels;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
+fn hsl2rgb_f64(h: f64, s: f64, l: f64) -> (f64, f64, f64) {
+    if s == 0. { (l, l, l) }
+    else {
+        fn pqt2v(p: f64, q: f64, mut t: f64) -> f64 {
+            if t < 0. { t += 1.; }
+            if t > 1. { t -= 1.; }
+            if t < (1./6.) { return p + (q - p) * 6. * t; }
+            if t < (1./2.) { return q; }
+            if t < (2./3.) { return p + (q - p) * (2./3. - t) * 6.; }
+            return p;
+        }
+        let mut q =  l * (1. - s);
+        if l < 0.5 { q = l * (1. + s); }
+        let p = 2. * l;
+        let r = pqt2v(q, p, h + 1./3.);
+        let g = pqt2v(q, p, h);
+        let b = pqt2v(q, p, h - 1./3.);
+        (r, g, b)
+    }
+}
+
+fn hsl2rgb_u8(h: u8, s: u8, l: u8) -> (u8, u8, u8) {
+    let (r, g, b) = hsl2rgb_f64((h as f64) / 255., (s as f64) / 255., (l as f64) / 255.);
+    ((r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8)
+}
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsys = sdl_context.video().unwrap();
@@ -23,8 +49,14 @@ fn main() {
     let mut lasty = 0;
 
     let mut events = sdl_context.event_pump().unwrap();
+    let mut i = 0f64;
 
     'main: loop {
+        let (r, g, b) = hsl2rgb_f64(i % 1., 1., 0.5);
+        i += 0.0001;
+        canvas.set_draw_color(pixels::Color::RGB((r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8));
+        canvas.clear();
+        canvas.present();
         for event in events.poll_iter() {
 
             match event {
