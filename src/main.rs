aff325a407c08b0a883aa35a601e31fff039f49a
Havard Haug <hau@vizrt.com>
2019-01-17 20:32:22 +0100

    Added moving characters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 extern crate sdl2;
+extern crate stb_image;
 
-use sdl2::pixels;
+use stb_image::image::LoadResult;
+use std::path::Path;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::rect::Point;
+use std::time::Duration;
+use sdl2::pixels;
+
+use stb_image::image;
 
 mod audio_engine;
 use audio_engine::AudioEngine;
@@ -33,63 +42,117 @@ fn hsl2rgb_u8(h: u8, s: u8, l: u8) -> (u8, u8, u8) {
     ((r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8)
 }
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsys = sdl_context.video().unwrap();
-    let window = video_subsys.window("GameJam 2019", 640, 480)
-        .position_centered()
-        .opengl()
-        .build()
-        .map_err(|e| e.to_string()).unwrap();
 
-    let mut canvas = window.into_canvas().accelerated().build().map_err(|e| e.to_string()).unwrap();
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    
+    let window = video_subsystem.window("SDL2", 1320, 768)
+        .position_centered().opengl().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let mut canvas = window.into_canvas()
+        .accelerated().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
 
-    let mut lastx = 0;
-    let mut lasty = 0;
+    canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
 
-    let mut events = sdl_context.event_pump().unwrap();
     let mut i = 0f64;
 
     let mut audio_engine = AudioEngine::new(sdl_context.audio().unwrap());
 
     audio_engine.play_sound_from_file("src/resources/music/personal_space.wav");
 
-    'main: loop {
-        let (r, g, b) = hsl2rgb_f64(i % 1., 1., 0.5);
-        i += 0.0001;
-        canvas.set_draw_color(pixels::Color::RGB((r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8));
-        canvas.clear();
-        canvas.present();
-        for event in events.poll_iter() {
+    let mut timer = sdl_context.timer()?;
 
+    let mut event_pump = sdl_context.event_pump()?;
+
+
+    let mut png_img = match image::load(Path::new("src/resources/image/characters.png")) {
+        LoadResult::ImageU8(bytes) => { bytes },
+        LoadResult::ImageF32(_) => panic!("Is float"),
+        _ => panic!("Failed to load image")
+    };
+    
+    
+    // animation sheet and extras are available from
+    // https://opengameart.org/content/a-platformer-in-the-forest
+    //let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/characters.bmp"))?;
+    let temp_surface = sdl2::surface::Surface::from_data(
+        &mut png_img.data,
+        png_img.width as u32,
+        png_img.height as u32,
+        png_img.width as u32 * 4,
+        sdl2::pixels::PixelFormatEnum::ABGR8888
+    ).unwrap();
+
+    let texture = texture_creator.create_texture_from_surface(&temp_surface)
+        .map_err(|e| e.to_string())?;
+
+    let frames_per_anim = 24;
+    let sprite_tile_size = (32,32);
+
+    // Baby - walk animation
+    let mut source_rect_0 = Rect::new(0, 0, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_0 = Rect::new(0, 0, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_0.center_on(Point::new(-64,120));
+
+    // King - walk animation
+    let mut source_rect_1 = Rect::new(0, 32, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_1 = Rect::new(0, 32, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_1.center_on(Point::new(0,240));
+
+    // Soldier - walk animation
+    let mut source_rect_2 = Rect::new(0, 64, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_2 = Rect::new(0, 64, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_2.center_on(Point::new(440,360));
+
+    // Snake - slither animation
+    let mut source_rect_3 = Rect::new(0, 96, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_3 = Rect::new(0, 64, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_3.center_on(Point::new(200, 480));
+
+    let mut running = true;
+    while running {
+        for event in event_pump.poll_iter() {
             match event {
-
-                Event::Quit {..} => break 'main,
-
-                Event::KeyDown {keycode: Some(keycode), ..} => {
-                    if keycode == Keycode::Escape {
-                        break 'main
-                    } else if keycode == Keycode::Space {
-                        println!("space down");
-                        canvas.present();
-                    }
-                }
-
-                Event::MouseButtonDown {x, y, ..} => {
-                    let color = pixels::Color::RGB(x as u8, y as u8, 255);
-                    lastx = x as i16;
-                    lasty = y as i16;
-                    println!("mouse btn down at ({},{})", x, y);
-                    canvas.present();
-                }
-
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    running = false;
+                },
                 _ => {}
             }
         }
+
+        let ticks = timer.ticks() as i32;
+
+        // set the current frame for time
+        source_rect_0.set_x(32 * ((ticks / 100) % frames_per_anim));
+        dest_rect_0.set_x(1 * ((ticks / 14) % 768) - 128);
+
+        source_rect_1.set_x(32 * ((ticks / 100) % frames_per_anim));
+        dest_rect_1.set_x((1 * ((ticks / 12) % 768) - 672) * -1);
+
+        source_rect_2.set_x(32 * ((ticks / 100) % frames_per_anim));
+        dest_rect_2.set_x(1 * ((ticks / 10) % 768) - 128);
+
+        source_rect_3.set_x(32 * ((ticks / 100 % 4)));
+        dest_rect_3.set_x((1 * ((ticks / 8 ) % 768) - 672) * -1);
+
+        canvas.clear();
+
+        // copy the frame to the canvas
+        canvas.copy_ex(&texture, Some(source_rect_0), Some(dest_rect_0), 0.0, None, false, false)?;
+        canvas.copy_ex(&texture, Some(source_rect_1), Some(dest_rect_1), 0.0, None, true, false)?;
+        canvas.copy_ex(&texture, Some(source_rect_2), Some(dest_rect_2), 0.0, None, false, false)?;
+        canvas.copy_ex(&texture, Some(source_rect_3), Some(dest_rect_3), 0.0, None, true, false)?;
+
+        let (r, g, b) = hsl2rgb_f64(i % 1., 1., 0.5);
+        i += 0.01;
+        canvas.set_draw_color(pixels::Color::RGB((r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8));
+
+        canvas.present();
+
+        std::thread::sleep(Duration::from_millis(100));
     }
 
+    Ok(())
 }
