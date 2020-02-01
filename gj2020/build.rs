680f455771ed90739f3af67b0cc1f2c4d3a5bdae
Marcin Mateusz Hanc <marcin.mateusz.hanc@gmail.com>
2020-02-01 13:34:04 +0100

    Create subpath in buildscript before build is actually don

--- gj2020/build.rs
+++ gj2020/build.rs
@@ -8,9 +8,11 @@ fn main() {
     // options.mirror_copy = true; // To mirror copy the whole structure of the source directory
 
     if !Path::new("./target/debug/assets").exists() {
+        std::fs::create_dir_all("./target/debug");
         dir::copy("./assets", "./target/debug", &options).unwrap();
     }
     if !Path::new("./target/release/assets").exists() {
+        std::fs::create_dir_all("./target/release");
         dir::copy("./assets", "./target/release", &options).unwrap();
     }
 }
