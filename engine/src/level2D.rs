8b75ad156b4727db632a12872e775cc36dc6fae2
Marius Tendeland Horne <Marius_t_h@hotmail.com>
2020-02-01 15:15:02 +0100

    Add saving to level2d

--- engine/src/level2D.rs
+++ engine/src/level2D.rs
@@ -4,9 +4,10 @@ use Engine;
 
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 use std::path::PathBuf;
 use vector::Vec2;
-
+use Error;
 
 #[derive(Serialize, Deserialize)]
 pub struct LevelInstance {
@@ -29,12 +30,14 @@ pub struct ObjectType {
 }
 
 pub struct Level2D {
-    pub level_instance: LevelInstance
+    pub level_instance: LevelInstance,
+    pub save_filename: String
 }
 
 impl Level2D {
     pub fn load_from_file(_ctx: &mut Engine, filename: &str) -> Level2D {
         let mut file = File::open(&filename).unwrap();
+        
         let mut data = String::new();
         file.read_to_string(&mut data).unwrap();
 
@@ -47,13 +50,29 @@ impl Level2D {
         image_folder.pop();
         image_folder.push("image");
 
+        let save_filename = filename.to_string();
+
         Level2D {
-            level_instance
+            level_instance,
+            save_filename
+        }
+    }
+
+    pub fn save_to_file(&self) -> Result<(), Error> {
+        if let Ok(mut f) = File::create(self.save_filename.clone()) {
+        
+            let instance = serde_json::to_string(&self.level_instance).unwrap();
+            f.write_all(&instance.as_bytes()).unwrap();
+
+            Ok(())
+        } else {
+            Err(Error::IO { path: Some(self.save_filename.clone()) })
         }
     }
 
     pub fn save(&mut self) {
-        println!("Saving to file        NOT");
+        println!("Saving to file");
+        self.save_to_file().unwrap();
     }
 }
 
